b9d94e6a25164d92d38ae8ff27d8a15030a925fe
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2022-10-09 19:19:30 +0530

    update greatest.cpp

--- greatest.cpp
+++ greatest.cpp
@@ -16,11 +16,11 @@ printf("z is the largest\n");
 else if(x==y&&y==z)
 printf("all are equal\n");
 else if(x==y&&y!=z&&x>z)
-printf("x and y is greater than z\n");
+printf("x and y are equal and greater than z\n");
 else if(x!=y&&y==z&&y>x)
-printf("y and z is greater than x\n");
+printf("y and z are equal and greater than x\n");
 else if(x==z&&x!=y&&x>y)
-printf("x and z is greater than y\n");
+printf("x and z are equal and greater than y\n");
 getch();
 return 0;
 }
