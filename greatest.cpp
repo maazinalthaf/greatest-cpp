f40b5744fc3b26afa392e941e77e739548851bcb
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2022-10-13 15:48:28 +0530

    Update greatest.cpp

--- greatest.cpp
+++ greatest.cpp
@@ -9,18 +9,20 @@ printf("Enter the values of x,y,z\n");
 scanf("%d%d%d",&x,&y,&z);
 if(x>y&&x>z)
 printf("x is the largest\n");
-else if(y>x&&y>z)
+  else if(y>x&&y>z)
 printf("y is the largest\n");
-else if(z>x&&z>y)
+  else if(z>x&&z>y)
 printf("z is the largest\n");
-else if(x==y&&y==z)
+  else if(x==y&&y==z)
 printf("all are equal\n");
-else if(x==y&&y!=z&&x>z)
+  else if(x==y&&y!=z&&x>z)
 printf("x and y are equal and greater than z\n");
-else if(x!=y&&y==z&&y>x)
+  else if(x!=y&&y==z&&y>x)
 printf("y and z are equal and greater than x\n");
-else if(x==z&&x!=y&&x>y)
+  else if(x==z&&x!=y&&x>y)
 printf("x and z are equal and greater than y\n");
+  else
+printf("Invalid\n");
 getch();
 return 0;
 }
